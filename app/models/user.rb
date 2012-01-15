21bb0da7d1eb8a70e7100ab1a78466f1896fc928
Robert J. Yates <robertjyates@gmail.com>
2012-01-15 02:11:27 -0800

    Added Log In page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create, :message => "can't be left blank"
   validates_presence_of :email
   validates_uniqueness_of :email
+
+  def self.authenticate(email, password)
+   user = find_by_email(email)
+   if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+     user
+   else
+     nil
+   end
+ end
   
   def encrypt_password
    if password.present?
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Auth::Application.routes.draw do
+ #  get "sessions/new"
+  get "log_in" => "sessions#new", :as => "log_in"
  #  get "users/new"
   get "sign_up" => "users#new", :as => "sign_up"
   root :to => "users#new"
   resources :users
+  resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
